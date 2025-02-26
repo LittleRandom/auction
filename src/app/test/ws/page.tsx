5f318faceaa638fb5abae683a2c03fa26f15c5a7
Quoc Vu <quocvu2640@gmail.com>
2025-02-26 16:59:34 -0500

    chore: removed index.html

--- src/app/test/ws/page.tsx
+++ src/app/test/ws/page.tsx
@@ -1,14 +1,27 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { useSocket } from "@/hooks/use-websockets";
+import { FormEvent, useState } from 'react';
 
 export default function Home() {
+    const [items, setItems] = useState({});
+
     const socket = useSocket()
+
+    socket.on("values", (values) => { setItems(values) })
+
+    const clickButton = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        socket.emit("readAll", (response: object) => {
+            setItems(response);
+        })
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={clickButton}>
                 <Button>Send</Button>
             </form>
+            <pre>{JSON.stringify(items, null, 2)}</pre>
         </div>
     );
 }
